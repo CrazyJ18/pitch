df6bcf6637a05525c228e4a33bc3c0d4d6922603
CrazyJ18 <jsteinke310@gmail.com>
2025-01-18 13:45:28 -0600

    draw card image

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import pygame
+import pygame, os
 
 pygame.init()
 screen = pygame.display.set_mode((1600, 900))
 clock = pygame.time.Clock()
 running = True
+card = pygame.image.load(os.path.join('assets', 'cards', 'ace_of_spades.webp'))
+card = pygame.transform.smoothscale_by(card.convert_alpha(), 0.3)
 
 while running:
     for event in pygame.event.get():
@@ -11,6 +13,17 @@ while running:
             running = False
     
     screen.fill("dark green")
+    screen.blit(card, (700, 400))
+
+    # deal hands of 9 cards
+    # bid
+    # winner declares suit
+    # other players discard all cards not in suit
+    # other players are dealt cards until 6 cards in their hands
+    # winner receives rest of deck
+    # winner discards out-of-suit cards until 6 cards in hand
+    # players take tricks
+    # round ends when all cards in suit have been played
 
     pygame.display.flip()
     clock.tick(60)
